eb0e33bd23a92c61e8998f60b42e4989b06550c5
r-m-hufford <ryan@rmhufford.com>
2021-03-31 19:42:45 -0400

    Streamzzzzzzz

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,42 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        return Arrays.stream(array)
+                .filter(element -> countElements(element) < maxNumberOfDuplications)
+                .toArray(Integer[]::new);
+
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return Arrays.stream(array)
+                .filter(element -> countElements(element) > exactNumberOfDuplications || countElements(element) < exactNumberOfDuplications)
+                .toArray(Integer[]::new);
+
+    }
+
+
+    public Integer countElements(Integer numberToCount) {
+        Integer valueToReturn = 0;
+        for (Integer number : array) {
+            if (number == numberToCount) {
+                valueToReturn++;
+            }
+        }
+        return valueToReturn;
+    }
+    //Arrays.stream(letters).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,37 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        return Arrays.stream(array)
+                .filter(element -> countElements(element) < maxNumberOfDuplications)
+                .toArray(String[]::new);
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return Arrays.stream(array)
+                .filter(element -> countElements(element) > exactNumberOfDuplications || countElements(element) < exactNumberOfDuplications)
+                .toArray(String[]::new);
+    }
+
+    public Integer countElements(String stringToCount) {
+        Integer valueToReturn = 0;
+        for (String string : array) {
+            if (string == stringToCount) {
+                valueToReturn++;
+            }
+        }
+        return valueToReturn;
+    }
 }
